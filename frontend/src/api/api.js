4970ecea5d1720c32612db7c147ec91facb82ecd
litao <18202886913@163.com>
2018-08-10 16:25:05 +0800

    创建项目

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // export const test = 'http://127.0.0.1:8000';
 // export const test = 'http://192.168.193.130:8000';
-export const test = 'http://120.79.232.23:8000';
+export const test = 'http://47.98.49.70:8000';
 // 获取公司黑名单
 export const getBlackList =  (headers, params) => {
     return axios.get(`${test}/api/company/blackList`, { params: params, headers:headers}).then(res => res.data); };
