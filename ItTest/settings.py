219055d96a678b1e95e731d35f33418933e3c8a6
litao <18202886913@163.com>
2018-08-21 10:25:58 +0800

    修复数据库连接错误

--- ItTest/settings.py
+++ ItTest/settings.py
@@ -122,23 +122,23 @@ WSGI_APPLICATION = 'ItTest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'test',
-#         'USER': 'root',
-#         'PASSWORD': '123456',
-#         'HOST': '172.16.153.83',
-#         # 'HOST': '47.98.49.70',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '172.16.153.83',
+        # 'HOST': '47.98.49.70',
+        'PORT': '3306',
+    }
+}
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/dist/static"),
